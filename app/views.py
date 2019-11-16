2fc8d6468d1d4bf038da5cc57046a96bf9d71bcf
margaret254 <maggiemwas91@gmail.com>
2019-11-17 00:29:48 +0300

    Hide the api key in the gitignore file

--- app/views.py
+++ app/views.py
@@ -4,9 +4,11 @@ from app import app
 # Views
 
 @app.route('/news/<int:news_id>')
-def index(news_id):
+def index():
     '''
     View news page function that returns the news details page and its data
     '''
 
-    return render_template('news.html',id = news_id)
+    title = 'Home - Welcome to Our News Online Appplication'
+
+    return render_template('index.html', title = title)
